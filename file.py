0b43bbc2ee01f1c988f4e357ac9fe69863df2310
M.Jawad Iqbal <jawadiqbal2006@gmail.com>
2025-11-27 21:07:25 +0500

    Added Calculator code

---
+++ file.py
@@ -0,0 +1,21 @@
+print("\t*** Welcome to Calculator App ***") 
+num1=float(input("Enter First Number: "))
+num2=float(input("Enter Second Number:"))
+while True:
+    print("\nSelect Operation")
+    oper=int(input("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n"))
+    print()
+    if(oper==1):
+        print(num1, "+" ,num2, "=" ,num1+num2)
+    elif(oper==2):
+        print(num1, "-" ,num2, "=" ,num1-num2)
+    elif(oper==3):
+        print(num1, "*" ,num2, "=" ,num1*num2)
+    elif(oper==4):
+        print(num1, "/" ,num2, "=" ,num1/num2)
+    else:
+        print("\nInvalid Selection Try again")
+    again=int(input("\nInput 1 to perform operation on same numbers\nInput 0 to exit the App\n"))
+    if(again==0):
+        break
+print("\nThank you for using this App, Good bye")
