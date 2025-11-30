0904b67005061cd052b2c9014a906f836f5e3d80
Hamza-Ali433 <hamzajatt177gmail.com>
2025-11-30 20:15:46 +0500

    Added a gussing number game

--- file.py
+++ file.py
@@ -19,3 +19,26 @@ while True:
     if(again==0):
         break
 print("\nThank you for using this App, Good bye")
+
+import random
+
+while True:
+    print("\n--- Number Guessing Game ---")
+    secret_number = random.randint(1, 50)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess a number between 1 and 50: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f" Correct! You guessed the number in {attempts} attempts.")
+            break
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
